a2b2ffede0eb991927a3ca5ef29ea511278b4cc9
holodilnik-24 <ivanromanof0921@gmail.com>
2025-04-05 14:38:49 +0300

    asd

---
+++ js/index.js
@@ -0,0 +1,22 @@
+// document.addEventListener("contextmenu", function(e) {
+//     e.preventDefault();
+// });
+
+// let isMoon = false; 
+
+// document.getElementById('theme-toggle').addEventListener('click', function() {
+//     const body = document.getElementById('body');
+//     const themeToggleImg = document.getElementById('theme-toggle');
+    
+//     if (isMoon) {
+//         body.classList.remove('dark-theme');
+//         body.classList.add('light-theme');
+//         themeToggleImg.src = "moon-icon.png"; 
+//     } else {
+//         body.classList.remove('light-theme');
+//         body.classList.add('dark-theme');
+//         themeToggleImg.src = "sun-icon.png"; 
+//     }
+    
+//     isMoon = !isMoon; 
+// });
